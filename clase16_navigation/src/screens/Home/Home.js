37371b69fc45e8fc5d845d3ef4b6b3ea358e4c25
alejandrodh <alejandro@digitalhouse.com>
2023-10-11 10:53:00 -0300

    Navigation

---
+++ clase16_navigation/src/screens/Home/Home.js
@@ -0,0 +1,25 @@
+import react, { Component } from 'react';
+import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+
+class Home extends Component {
+    constructor(){
+        super()
+        this.state={
+      
+        }
+    }
+
+
+
+    render(){
+        return(
+            <View>
+                <Text>HOME</Text>
+            </View>
+        )
+    }
+}
+
+
+
+export default Home;
